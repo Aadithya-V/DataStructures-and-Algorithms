bcb20a6b60bb8fb2027b6c490aff2c433adc160a
Aadithya V <Aadithya-V@users.noreply.github.com>
2022-01-03 23:57:51 +0530

    merge_sort

--- Sorting/merge_sort.c
+++ Sorting/merge_sort.c
@@ -39,7 +39,8 @@ void MergeSort(int *A,int n) {
 
 	MergeSort(L,mid);  // sorting the left subarray
 	MergeSort(R,n-mid);  // sorting the right subarray
-	Merge(A,L,mid,R,n-mid);  // Merging L and R into A as sorted list.
+	
+    Merge(A,L,mid,R,n-mid);  // Merging L and R into A as sorted list.
         free(L);
         free(R);
 }
@@ -47,19 +48,19 @@ void MergeSort(int *A,int n) {
 int main() {
 	/* Code to test the MergeSort function. */
 	
-	int A[] = {6,2,3,1,9,10,15,13,12,17}; // creating an array of integers. 
+	int Arr[] = {6,2,3,1,9,10,15,13,12,17}; // creating an array of integers. 
 	int i,numberOfElements;
 
 	// finding number of elements in array as size of complete array in bytes divided by size of integer in bytes. 
 	// This won't work if array is passed to the function because array
 	// is always passed by reference through a pointer. So sizeOf function will give size of pointer and not the array.
 	// Watch this video to understand this concept - http://www.youtube.com/watch?v=CpjVucvAc3g  
-	numberOfElements = sizeof(A)/sizeof(A[0]); 
+	numberOfElements = sizeof(Arr)/sizeof(Arr[0]); 
 
 	// Calling merge sort to sort the array. 
-	MergeSort(A,numberOfElements);
+	MergeSort(Arr,numberOfElements);
 
 	//printing all elements in the array once its sorted.
-	for(i = 0;i < numberOfElements;i++) printf("%d ",A[i]);
+	for(i = 0;i < numberOfElements;i++) printf("%d ",Arr[i]);
 	return 0;
 }
